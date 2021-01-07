ebfe724023f76a758ead16defa6f025b8390ccdd
dulcerussel <54066608+dulcerussel@users.noreply.github.com>
2021-01-07 14:21:20 +0800

    Business Calculation

--- app/src/main/java/com/example/aboitez/MyBusiness.java
+++ app/src/main/java/com/example/aboitez/MyBusiness.java
@@ -15,11 +15,14 @@ public class MyBusiness extends Application {
     private float sunday;
     private float weeklysales;
     private float dailyavesales;
+    private float weeklystandsales;
     private float dailystandavesales;
     private int actualmarkup;
+    private float netprofit;
+    private float netcost;
 
 
-    public MyBusiness(int id, int application_id, float monday, float tuesday, float wednesday, float thursday, float friday, float saturday, float sunday, float weeklysales, float dailyavesales, float dailystandavesales, int actualmarkup) {
+    public MyBusiness(int id, int application_id, float monday, float tuesday, float wednesday, float thursday, float friday, float saturday, float sunday, float weeklysales, float dailyavesales, float weeklystandsales, float dailystandavesales, int actualmarkup, float netprofit, float netcost) {
         this.id = id;
         this.application_id = application_id;
         this.monday = monday;
@@ -31,8 +34,11 @@ public class MyBusiness extends Application {
         this.sunday = sunday;
         this.weeklysales = weeklysales;
         this.dailyavesales = dailyavesales;
+        this.weeklystandsales = weeklystandsales;
         this.dailystandavesales = dailystandavesales;
         this.actualmarkup = actualmarkup;
+        this.netprofit = netprofit;
+        this.netcost = netcost;
     }
 
     public MyBusiness() {
@@ -52,9 +58,51 @@ public class MyBusiness extends Application {
                 ", sunday=" + sunday +
                 ", weeklysales=" + weeklysales +
                 ", dailyavesales=" + dailyavesales +
+                ", weeklystandsales=" + weeklystandsales +
                 ", dailystandavesales=" + dailystandavesales +
                 ", actualmarkup=" + actualmarkup +
+                ", netprofit=" + netprofit +
+                ", netcost=" + netcost +
                 '}';
+    }
+
+    public void CalculateWeeklySales(){
+        setWeeklysales(this.monday+this.tuesday+this.wednesday+this.thursday+this.friday+this.saturday+this.sunday);
+    }
+
+    public void CalculateAverageSales(){
+        float x = getWeeklysales();
+        setDailyavesales(x/7);
+    }
+
+    public void CalculateWeeklyStandardSales(float cost){
+            float x = (float)1.25;
+            setWeeklysales(cost*x);
+    }
+
+    public void CalculateStandardAverageSales(){
+        float ave =  getWeeklysales();
+        ave = ave / 7;
+        setDailystandavesales(ave);
+    }
+
+
+    public void CalculateWeeklyNetProfit(){
+        float x = getNetcost();
+        float y = (float)0.25;
+        setNetprofit(x*y);
+    }
+
+    public void CalculateTotalCost(float sum){
+        setNetcost(this.weeklysales + sum);
+    }
+
+    public float getWeeklystandsales() {
+        return weeklystandsales;
+    }
+
+    public void setWeeklystandsales(float weeklystandsales) {
+        this.weeklystandsales = weeklystandsales;
     }
 
     public int getId() {
@@ -159,5 +207,21 @@ public class MyBusiness extends Application {
 
     public void setActualmarkup(int actualmarkup) {
         this.actualmarkup = actualmarkup;
+    }
+
+    public float getNetcost() {
+        return netcost;
+    }
+
+    public void setNetcost(float netcost) {
+        this.netcost = netcost;
+    }
+
+    public float getNetprofit() {
+        return netprofit;
+    }
+
+    public void setNetprofit(float netprofit) {
+        this.netprofit = netprofit;
     }
 }
