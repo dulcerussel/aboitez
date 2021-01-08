e7cb72a02de8c54686065eed90f22a76cec6a695
dulcerussel <54066608+dulcerussel@users.noreply.github.com>
2021-01-08 19:09:10 +0800

    7:09

--- app/src/main/java/com/example/aboitez/householdincome.java
+++ app/src/main/java/com/example/aboitez/householdincome.java
@@ -5,14 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class householdincome extends AppCompatActivity {
     private Button button, button2;
+
+    private ListView lv;
+    private EditText Et;
+
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_householdincome);
+
+        this.lv = findViewById(R.id.listview2);
+        this.Et = findViewById(R.id.editTextNumber);
+
         button2 = (Button) findViewById(R.id.btnNext1);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +42,11 @@ public class householdincome extends AppCompatActivity {
                 openDialog();
             }
         });
+
+        list  = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
+
+        lv.setAdapter(adapter);
     }
 
     private void openHE() {
@@ -35,7 +55,11 @@ public class householdincome extends AppCompatActivity {
     }
 
     private void openDialog() {
-        householdincomdialog householdincomdialog =  new householdincomdialog();
-        householdincomdialog.show(getSupportFragmentManager(),"example dialog");
+//        householdincomdialog householdincomdialog =  new householdincomdialog();
+//        householdincomdialog.show(getSupportFragmentManager(),"example dialog");
+
+        String result = Et.getText().toString();
+        list.add(result);
+        adapter.notifyDataSetChanged();
     }
 }
