9d3adbfcbb5d45af22a5927560ee3d81ce91edeb
dulcerussel <54066608+dulcerussel@users.noreply.github.com>
2021-01-08 15:20:21 +0800

    view total compute

--- app/src/main/java/com/example/aboitez/totalCompute.java
+++ app/src/main/java/com/example/aboitez/totalCompute.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class totalCompute extends AppCompatActivity {
     private Button button5;
+    private TextView txttotalhousehold, txttotalbusiness,txttotalnetcombineincome,txtadc,loanterms,txtadcxterms,txtmaxloanamount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,11 +18,45 @@ public class totalCompute extends AppCompatActivity {
 
         MyBusiness business = (MyBusiness) getApplicationContext();
         MyApplicant applicant = (MyApplicant) getApplicationContext();
+        MyApplication application = (MyApplication) getApplicationContext();
         MyOtherOperatingCost otherOperatingCost = (MyOtherOperatingCost) getApplicationContext();
         MyHousehold household = (MyHousehold) getApplicationContext();
         MyOperatingCost operatingCost = (MyOperatingCost) getApplicationContext();
         MyHouseholdExpense householdExpense = (MyHouseholdExpense) getApplicationContext();
         MyHouseholdIncome householdIncome = (MyHouseholdIncome) getApplicationContext();
+
+
+        this.txttotalhousehold = (TextView) findViewById(R.id.textView47);
+        this.txttotalbusiness = (TextView) findViewById(R.id.textView46);
+        this.txttotalnetcombineincome = (TextView) findViewById(R.id.textView45);
+        this.txtadc = (TextView) findViewById(R.id.textView44);
+        this.loanterms = (TextView) findViewById(R.id.textView43);
+        this.txtadcxterms = (TextView) findViewById(R.id.textView42);
+        this.txtmaxloanamount = (TextView) findViewById(R.id.textView41);
+
+        //calculate
+        float x = business.getNetprofit();
+        float y = household.getNethouseincome();
+
+
+
+        application.CalculateNetBusinessIncome(x);
+        application.CalculateNetCombineIncome(x,y);
+
+        float w = application.getTotalnetcombineincome();
+
+        application.CalculateAdjustedDebtCapacity(w);
+        application.CalculateADCxTerms();
+        application.MaxLoanAmountAvailment();
+
+        txttotalhousehold.setText(String.valueOf(application.getHousehold_total()));
+        txttotalbusiness.setText(String.valueOf(application.getBusiness_total()));
+        txttotalnetcombineincome.setText(String.valueOf(application.getTotalnetcombineincome()));
+        txtadc.setText(String.valueOf(application.getAdc()));
+        loanterms.setText(String.valueOf(application.getLoanterm()));
+        txtadcxterms.setText(String.valueOf(application.getAdcxterms()));
+        txtmaxloanamount.setText(String.valueOf(application.getMaxloanamount()));
+
         button5 = (Button) findViewById(R.id.btnDone);
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
